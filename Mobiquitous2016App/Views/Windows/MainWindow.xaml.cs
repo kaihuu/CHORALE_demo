ede6f574eb6d1c9131c779f9a55975d05ae4e3db
Daiki-Kawanuma <daiki.kawanuma@gmail.com>
2016-10-25 18:16:45 +0900

    change namespace

--- Mobiquitous2016App/Views/Windows/MainWindow.xaml.cs
+++ Mobiquitous2016App/Views/Windows/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Mobiquitous2016App.Views
+namespace Mobiquitous2016App.Views.Windows
 {
     /* 
 	 * ViewModelからの変更通知などの各種イベントを受け取る場合は、PropertyChangedWeakEventListenerや
