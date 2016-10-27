a749100c924790b28549b0e1c7697e7c279bfb8c
Daiki-Kawanuma <daiki.kawanuma@gmail.com>
2016-10-27 17:24:27 +0900

    implement SemanticLink

---
+++ Mobiquitous2016App/Daos/SemanticLinkDao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mobiquitous2016App.Models.EcologModels;
+
+namespace Mobiquitous2016App.Daos
+{
+    public class SemanticLinkDao
+    {
+        public static List<SemanticLink> GetOutwardSemanticLinks()
+        {
+            return null;
+        }
+
+        public static List<SemanticLink> GetHomewardSemanticLinks()
+        {
+            return null;
+        }
+    }
+}
--- Mobiquitous2016App/Models/EcologModels/SemanticLink.cs
+++ Mobiquitous2016App/Models/EcologModels/SemanticLink.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Mobiquitous2016App.Daos;
 
 namespace Mobiquitous2016App.Models.EcologModels
 {
     public class SemanticLink
     {
+        public static IList<SemanticLink> OutwardSemanticLinks => SemanticLinkDao.GetOutwardSemanticLinks();
+
+        public static IList<SemanticLink> HomewardSemanticLinks => SemanticLinkDao.GetHomewardSemanticLinks();
+
         public int SemanticLinkId { get; set; }
         public int DriverId { get; set; }
         public List<Link> Links { get; set; }
