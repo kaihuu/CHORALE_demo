406ce5594e8188067a40b917bd6a9f78b914a3d0
Daiki-Kawanuma <daiki.kawanuma@gmail.com>
2016-10-27 17:37:09 +0900

    implement DatabaseConnection

---
+++ Mobiquitous2016App/Daos/DatabaseConnection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobiquitous2016App.Daos
+{
+    public static class DatabaseConnection
+    {
+        public static string ConnectionString => GetConnectionString();
+
+        private static string GetConnectionString()
+        {
+            var builder = new SqlConnectionStringBuilder()
+            {
+                DataSource = "ECOLOGDB",
+                InitialCatalog = "ECOLOGDBver2",
+                IntegratedSecurity = true,
+                ConnectTimeout = 15
+            };
+
+            return builder.ToString();
+        }
+    }
+}
