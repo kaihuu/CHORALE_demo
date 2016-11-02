78072ffa68fb8de895c697f7a98eaac64e8d9a40
Daiki <daiki.kawanuma@gmail.com>
2016-11-02 19:22:28 +0900

    implement ECGsPage

---
+++ Mobiquitous2016App/ViewModels/PageViewModels/ECGsPageViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+using Livet;
+using Livet.Commands;
+using Livet.Messaging;
+using Livet.Messaging.IO;
+using Livet.EventListeners;
+using Livet.Messaging.Windows;
+
+using Mobiquitous2016App.Models;
+using Mobiquitous2016App.Models.GraphModels;
+using Mobiquitous2016App.ViewModels.WindowViewModels;
+
+namespace Mobiquitous2016App.ViewModels.PageViewModels
+{
+    // ReSharper disable once InconsistentNaming
+    public class ECGsPageViewModel : ViewModel
+    {
+        private readonly GraphWindowViewModel _parentViewModel;
+
+        #region GraphData変更通知プロパティ
+        private IList<GraphDatum> _GraphData;
+
+        public IList<GraphDatum> GraphData
+        {
+            get
+            { return _GraphData; }
+            set
+            { 
+                if (_GraphData == value)
+                    return;
+                _GraphData = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+        public ECGsPageViewModel(GraphWindowViewModel parentViewModel)
+        {
+            _parentViewModel = parentViewModel;
+        }
+
+        public void Initialize()
+        {
+            GraphData = _parentViewModel.GraphDataList;
+        }
+    }
+}
---
+++ Mobiquitous2016App/Views/Pages/ECGsPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mobiquitous2016App.Views.Pages
+{
+    /// <summary>
+    /// ECGsPage.xaml の相互作用ロジック
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
+    public partial class ECGsPage : Page
+    {
+        public ECGsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
