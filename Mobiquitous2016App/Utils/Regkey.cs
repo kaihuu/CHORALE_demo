56173363c48185b2446b591bdceb80eb3d547910
Toshiaki Uemura <uemura-toshiaki-xh@ynu.jp>
2017-05-31 19:07:44 +0900

    add change regkey method

---
+++ Mobiquitous2016App/Utils/Regkey.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobiquitous2016App.Utils
+{
+    public class Regkey
+    {
+        Microsoft.Win32.RegistryKey regkey = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(FEATURE_BROWSER_EMULATION);
+        const string FEATURE_BROWSER_EMULATION = @"Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION";
+        string process_name = System.Diagnostics.Process.GetCurrentProcess().ProcessName + ".exe";
+        string process_dbg_name = System.Diagnostics.Process.GetCurrentProcess().ProcessName + ".vshost.exe";
+
+        public void changeRegkey()
+        {
+            regkey.SetValue(process_name, 11001, Microsoft.Win32.RegistryValueKind.DWord);
+            regkey.SetValue(process_dbg_name, 11001, Microsoft.Win32.RegistryValueKind.DWord);
+        }
+    }
+}
