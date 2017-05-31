6047f72e7ed72014bc5476887052c7b09e1cce5e
Toshiaki Uemura <uemura-toshiaki-xh@ynu.jp>
2017-05-31 22:06:46 +0900

    modify

--- Mobiquitous2016App/ViewModels/WindowViewModels/SelectWindowViewModel.cs
+++ Mobiquitous2016App/ViewModels/WindowViewModels/SelectWindowViewModel.cs
@@ -12,55 +12,73 @@ using Livet.EventListeners;
 using Livet.Messaging.Windows;
 
 using Mobiquitous2016App.Models;
+using Mobiquitous2016App.Models.EcologModels;
+using Mobiquitous2016App.Utils;
+using Mobiquitous2016App.Daos;
+
 
 namespace Mobiquitous2016App.ViewModels.WindowViewModels
 {
     public class SelectWindowViewModel : ViewModel
     {
-        /* コマンド、プロパティの定義にはそれぞれ 
-         * 
-         *  lvcom   : ViewModelCommand
-         *  lvcomn  : ViewModelCommand(CanExecute無)
-         *  llcom   : ListenerCommand(パラメータ有のコマンド)
-         *  llcomn  : ListenerCommand(パラメータ有のコマンド・CanExecute無)
-         *  lprop   : 変更通知プロパティ(.NET4.5ではlpropn)
-         *  
-         * を使用してください。
-         * 
-         * Modelが十分にリッチであるならコマンドにこだわる必要はありません。
-         * View側のコードビハインドを使用しないMVVMパターンの実装を行う場合でも、ViewModelにメソッドを定義し、
-         * LivetCallMethodActionなどから直接メソッドを呼び出してください。
-         * 
-         * ViewModelのコマンドを呼び出せるLivetのすべてのビヘイビア・トリガー・アクションは
-         * 同様に直接ViewModelのメソッドを呼び出し可能です。
-         */
 
-        /* ViewModelからViewを操作したい場合は、View側のコードビハインド無で処理を行いたい場合は
-         * Messengerプロパティからメッセージ(各種InteractionMessage)を発信する事を検討してください。
-         */
 
-        /* Modelからの変更通知などの各種イベントを受け取る場合は、PropertyChangedEventListenerや
-         * CollectionChangedEventListenerを使うと便利です。各種ListenerはViewModelに定義されている
-         * CompositeDisposableプロパティ(LivetCompositeDisposable型)に格納しておく事でイベント解放を容易に行えます。
-         * 
-         * ReactiveExtensionsなどを併用する場合は、ReactiveExtensionsのCompositeDisposableを
-         * ViewModelのCompositeDisposableプロパティに格納しておくのを推奨します。
-         * 
-         * LivetのWindowテンプレートではViewのウィンドウが閉じる際にDataContextDisposeActionが動作するようになっており、
-         * ViewModelのDisposeが呼ばれCompositeDisposableプロパティに格納されたすべてのIDisposable型のインスタンスが解放されます。
-         * 
-         * ViewModelを使いまわしたい時などは、ViewからDataContextDisposeActionを取り除くか、発動のタイミングをずらす事で対応可能です。
-         */
+        #region SemanticLinks変更通知プロパティ
+        private IList<SemanticLink> _SemanticLinks;
 
-        /* UIDispatcherを操作する場合は、DispatcherHelperのメソッドを操作してください。
-         * UIDispatcher自体はApp.xaml.csでインスタンスを確保してあります。
-         * 
-         * LivetのViewModelではプロパティ変更通知(RaisePropertyChanged)やDispatcherCollectionを使ったコレクション変更通知は
-         * 自動的にUIDispatcher上での通知に変換されます。変更通知に際してUIDispatcherを操作する必要はありません。
-         */
+        public IList<SemanticLink> SemanticLinks
+        {
+            get
+            { return _SemanticLinks; }
+            set
+            { 
+                if (_SemanticLinks == value)
+                    return;
+                _SemanticLinks = value;
+                RaisePropertyChanged("SemanticLinks");
+            }
+        }
+        #endregion
+
+
+        #region direction変更通知プロパティ
+        private TripDirection _direction;
+
+        public TripDirection direction
+        {
+            get
+            { return _direction; }
+            set
+            { 
+                if (_direction == value)
+                    return;
+                _direction = value;
+                RaisePropertyChanged("direction");
+            }
+        }
+        #endregion
+
 
         public void Initialize()
         {
+            Regkey reg = new Regkey();
+            reg.changeRegkey();
+            
+            if()
+        }
+
+        public void SetOutwardSemanticLinks()
+        {
+            _direction = new TripDirection { Direction = "outward" };
+            SemanticLinks = SemanticLinkDao.OutwardSemanticLinks;
+            //InvokeScript("initialize", null);
+        }
+
+        public void SetHomewardSemanticLinks()
+        {
+            _direction = new TripDirection { Direction = "homeward" };
+            SemanticLinks = SemanticLinkDao.HomewardSemanticLinks;
+            //InvokeScript("initialize", null);
         }
     }
 }
